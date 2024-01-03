8bdeb7ef2e34f264ee40b96d674ec397898bb44d
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-01-03 10:01:04 -0500

    added some comments where we will need the login logic that takes user to /bookshelf page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,20 @@
+const { response } = require('express');
+
 const router = require('express').Router();
 
 router.get('/', async(req, res) => {
     res.render('homepage');
 });
+
+router.get('/login', async(req, res) => {
+    res.render('loginpage');
+});
+
+// for user to get to bookshelf aka dashboard
+router.get('/bookshelf', (req, res) => {
+  // check if the user is logged in
+  // if the login function response is good
+  // send user to /bookshelf location as logged in
+  });
 
 module.exports = router;
