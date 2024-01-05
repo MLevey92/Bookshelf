d03ef5482a57bc2bf035e0473b59571c07dc0392
MLevey92 <mlevey92@gmail.com>
2024-01-04 22:41:01 -0500

    trying to append search results to result div

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,7 +65,9 @@ router.get("/bookshelf", async (req, res) => {
 
 // TODO: CREATE ROUTE FOR /findbooks
 router.get("/findbooks", async (req, res) => {
-  res.render("findbooks");
+  res.render("findbooks", {
+    logged_in: req.session.logged_in
+  });
 });
 
 module.exports = router;
