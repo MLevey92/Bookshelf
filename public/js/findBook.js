47cc079a6b201567b2043268cd3f262995f8da2e
MLevey92 <mlevey92@gmail.com>
2024-01-07 11:07:14 -0500

    search functionality works, but needs to be cleaned up

--- public/js/findBook.js
+++ public/js/findBook.js
@@ -7,9 +7,9 @@ function formHandler(event) {
     event.preventDefault();
 
     const searchBy = getRadioValue();
-    const queryURL = `https://openlibrary.org/search.json?${searchBy}=${spacesToPlusesAndTrim(inputEl.value)}`;
+    const queryURL = `https://openlibrary.org/search.json?${searchBy}=${spacesToPlusesAndTrim(inputEl.value)}&subject=book&fields=title,cover_edition_key,author_name,author_key,first_sentence,first_publish_year,ratings_average`;
     
-    fetchData(queryURL);
+    appendResults(queryURL);
 }
 
 async function fetchData(url) {
@@ -23,11 +23,72 @@ async function fetchData(url) {
         let data = await response.json();
         data = data.docs;
 
-        console.log(data);
+        return data;
     } catch (error) {
         console.error("Error during fetch:", error.message);
     }
 
+}
+
+async function appendResults(queryURL) {
+  try {
+    const results = await fetchData(queryURL);
+
+    const resultArray = results.map((book) => ({
+      title: book.title,
+      cover_edition_key: book.cover_edition_key,
+      first_publish_year: book.first_publish_year,
+      ratings_average: book.ratings_average
+        ? roundToTenths(book.ratings_average)
+        : null,
+      first_sentence: book.first_sentence ?? null,
+      author_key: book.author_key ? book.author_key[0] : null,
+      author_name: book.author_name
+        ? book.author_name[0] === "n/a"
+          ? null
+          : book.author_name[0]
+        : null,
+    }));
+
+    let cardFormats = "";
+
+    for (const book of resultArray) {
+      // TODO: give the stuff in cardFormat classes that you can style in the CSS
+      const cardFormat = `
+        <div class="seed-card">
+          <h4>Title: ${book.title}</h4>
+          <img src="https:covers.openlibrary.org/b/olid/${
+            book.cover_edition_key
+          }-M.jpg"/>
+          <p>Year: ${book.first_publish_year}</p>
+          <p>Rating: ${book.ratings_average}</p>
+          <!-- cutting first sentence to make search sleeker
+          ${
+            book.first_sentence !== null
+              ? `<p>First Sentence: ${book.first_sentence}</p>`
+              : ""
+          }
+          -->
+          <p>Author: ${book.author_name}</p>
+          {{#if logged_in}}
+            <p>add book</p>
+          {{else}}
+            <p>login to add book</p>
+          {{/if}}
+        </div>`;
+
+      cardFormats += cardFormat;
+    }
+
+    const featuredBooks = document.querySelector("#results");
+    featuredBooks.innerHTML = cardFormats; // Update innerHTML once
+
+    // Console logging the already clean array
+    console.log(resultArray);
+  } catch (error) {
+    console.error("Error getting books", error.message);
+    return []; // Return an empty array in case of an error
+  }
 }
 
 //Get value of checked radio button, used in query
@@ -41,12 +102,19 @@ function getRadioValue() {
     }
 }
 
+
+//helper functions
 function spacesToPlusesAndTrim(inputString) {
   // Trimming whitespace from both ends first
   inputString = inputString.trim();
   // Use the replace method with a regular expression to replace all spaces with '+'
   const resultString = inputString.replace(/ +/g, "+");
   return resultString;
+}
+
+// ! Lil function to turn 4.7619047 into 4.8
+function roundToTenths(number) {
+  return Number(number.toFixed(1));
 }
 
 formEl.addEventListener("submit", formHandler)
