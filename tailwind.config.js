50785527b63f378012e167df00742211af6dcc3a
MLevey92 <mlevey92@gmail.com>
2024-01-08 19:56:56 -0500

    added tailwind framework

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/findbooks.handlebars',
+            './public/bookshelf.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
