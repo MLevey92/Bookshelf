455448ae190a7cf00f7aeb37631e39848d32b6b7
MLevey92 <mlevey92@gmail.com>
2024-01-09 19:36:51 -0500

    added borders and button styling to homepage cards

--- public/js/homepageFunctions/seedHomepage.js
+++ public/js/homepageFunctions/seedHomepage.js
@@ -42,7 +42,7 @@ async function processBooks() {
               : ""
           }
           <p>Author: ${book.author_name}</p>
-          <button class="save-button">Save this!</button>
+          <button class="save-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-1/3">Save this!</button>
         </div>`;
 
       cardFormats += cardFormat;
